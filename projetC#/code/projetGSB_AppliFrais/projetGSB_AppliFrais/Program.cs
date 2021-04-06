9ca7b58a1d5c8941ae2318c36668065b3e442674
Loic Martin <loic.mart@outlook.fr>
2021-04-06 14:52:47 +0200

    ajout de la partie C#

---
+++ projetC#/code/projetGSB_AppliFrais/projetGSB_AppliFrais/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace projetGSB_AppliFrais
+
+{
+    public abstract class Program
+    {
+
+
+        public static void Main()
+        {
+
+            DateTime dateJour = DateTime.Now;
+
+            // Verification qu'on se trouve au debut du mois
+            if (Int32.Parse(dateJour.ToString("dd")) <= 10)
+            {
+                // On enlve 1 an si on recupere le mois de l'annee precedente
+                String mois = GestionDate.getMoisPrecedent(dateJour);
+                String annee = "";
+
+                // On recupere l'annee
+                if (mois == "12")
+                {
+                    annee = DateTime.Now.AddYears(-1).ToString("yyyy");
+
+                } else {
+                    annee = DateTime.Now.ToString("yyyy");
+                }
+
+                // on met à jour la base de données
+                string sql = "UPDATE fichefrais SET idEtat = 'CL', datemodif ='"+ dateJour.ToString("yyyy-MM-dd")+"' WHERE mois='" + annee + mois+ "' AND idetat='CR'";
+                DBUtils.addUpdateDeleteQuery(sql);
+            }
+
+            // Verification qu'on se trouve apres le 20 du moins
+            if (Int32.Parse(DateTime.Now.ToString("dd")) >= 20)
+            {
+                String mois = GestionDate.getMoisPrecedent(dateJour);
+                String annee = "";
+
+                // On recupere l'annee
+                if (mois == "12")
+                {
+                    annee = DateTime.Now.AddYears(-1).ToString("yyyy");
+                } else {
+                    annee = DateTime.Now.ToString("yyyy");
+
+                }
+
+                // on met à jour la base de données
+                string sql = "UPDATE fichefrais SET idEtat = 'RB', datemodif ='" + dateJour.ToString("yyyy-MM-dd") + "' WHERE mois='" + annee + mois + "' AND idetat='VA'";
+
+                DBUtils.addUpdateDeleteQuery(sql);
+            }
+
+
+        }
+
+    }
+}
---
+++ projetC#/code/projetGSB_AppliFrais/serviceGSB/Service1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using projetGSB_AppliFrais;
+
+namespace serviceGSB
+{
+    public partial class serviceGsb : ServiceBase
+    {
+        private Timer t = null;
+
+        public serviceGsb()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            t = new Timer(30000); // Timer de 30 secondes.
+            t.Elapsed += new ElapsedEventHandler(t_Elapsed);
+            t.Start();
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override void OnShutdown()
+        {
+            base.OnShutdown();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+
+        protected void t_Elapsed(object sender, EventArgs e)
+        {
+            projetGSB_AppliFrais.Program.Main();
+        }
+    }
+
+
+}
+
+
